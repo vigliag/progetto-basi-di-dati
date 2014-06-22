5e1eacc24d1f5920db7821db114e22853b942b9d
Gabriele Viglianisi <vigliag@gmail.com>
2014-06-22 10:01:06 +0200

    dashboard working

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,13 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin!
   layout "admin"
+
   def index
+  	@shows = Show.includes(:movie)
+  	.where(start: DateTime.now.beginning_of_day.. DateTime.now.end_of_day)
+  	.select("shows.*, count(tickets.id) as ticket_count")
+    .joins("left outer join tickets on tickets.show_id = shows.id")
+    .group('shows.id')
   end
 
-  def shows_params
-      params.require(:show).permit(:movie_id, :start, :price, :screen_id)
-    end
 end
